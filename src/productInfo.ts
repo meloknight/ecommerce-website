12448d60bb1cec6f9dd6eeba396bebdb60204f2e
brody <49502879+meloknight@users.noreply.github.com>
2024-03-15 13:00:24 -0700

    Add the remainder of the images to the public assets folder. Create half of the product info objects in productInfo.ts

--- src/productInfo.ts
+++ src/productInfo.ts
@@ -1,24 +1,167 @@
 export const productInfo: any[] = [
   {
     productId: 1,
-    productName: "fuzzy chair",
-    productPrice: 423,
-    productTexture: "synthetic wool",
-    productWeightInLbs: 40,
-    productSize: "60cm x 60cm",
-    firstProductImage: "#",
-    secondProductImage: "#",
-    thirdProductImage: "#",
+    productType: "Lamps",
+    productName: "Black and White Lamp",
+    productPrice: 120,
+    productTexture: "Smooth metal",
+    productWeightInLbs: 4,
+    productSize: "10cm x 10cm",
+    firstProductImage:
+      "public/assets/product-images/black-and-white-lamp/black-and-white-lamp-1.jpeg",
+    secondProductImage:
+      "public/assets/product-images/black-and-white-lamp/black-and-white-lamp-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/black-and-white-lamp/black-and-white-lamp-3.webp",
   },
   {
-    productId: 1,
-    productName: "fuzzy chair",
-    productPrice: 423,
-    productTexture: "synthetic wool",
-    productWeightInLbs: 40,
+    productId: 2,
+    productType: "Skin Care",
+    productName: "Body Oil 50mL",
+    productPrice: 18,
+    productTexture: "Oil",
+    productWeightInLbs: 0.4,
+    productSize: "5cm x 5cm",
+    firstProductImage:
+      "public/assets/product-images/body-oil-50ml/body-oil-50ml-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/body-oil-50ml/body-oil-50ml-2.jpg",
+    thirdProductImage:
+      "public/assets/product-images/body-oil-50ml/body-oil-50ml-3.jpg",
+  },
+  {
+    productId: 3,
+    productType: "Skin Care",
+    productName: "Body Oil 200mL",
+    productPrice: 60,
+    productTexture: "Oil",
+    productWeightInLbs: 1.6,
+    productSize: "10cm x 10cm",
+    firstProductImage:
+      "public/assets/product-images/body-oil-200ml/body-oil-200ml-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/body-oil-200ml/body-oil-200ml-2.jpg",
+    thirdProductImage:
+      "public/assets/product-images/body-oil-200ml/body-oil-200ml-3.jpg",
+  },
+  {
+    productId: 4,
+    productType: "Chairs",
+    productName: "Comfy Chair",
+    productPrice: 320,
+    productTexture: "Textile",
+    productWeightInLbs: 50,
     productSize: "60cm x 60cm",
-    firstProductImage: "#",
-    secondProductImage: "#",
-    thirdProductImage: "#",
+    firstProductImage:
+      "public/assets/product-images/comfy-chair/comfy-chair-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/comfy-chair/comfy-chair-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/comfy-chair/comfy-chair-3.webp",
+  },
+  {
+    productId: 5,
+    productType: "Chairs",
+    productName: "Copenhagen Armchair",
+    productPrice: 540,
+    productTexture: "Leather",
+    productWeightInLbs: 70,
+    productSize: "60cm x 60cm",
+    firstProductImage:
+      "public/assets/product-images/copenhagen-armchair/copenhagen-armchair-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/copenhagen-armchair/copenhagen-armchair-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/copenhagen-armchair/copenhagen-armchair-3.webp",
+  },
+  {
+    productId: 6,
+    productType: "Furniture",
+    productName: "Cube Lolo Vase Black",
+    productPrice: 40,
+    productTexture: "Smooth plastic",
+    productWeightInLbs: 4,
+    productSize: "15cm x 15cm",
+    firstProductImage:
+      "public/assets/product-images/cube-lolo-vase-black/cube-lolo-vase-black-1.png",
+    secondProductImage:
+      "public/assets/product-images/cube-lolo-vase-black/cube-lolo-vase-black-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/cube-lolo-vase-black/cube-lolo-vase-black-3.webp",
+  },
+  {
+    productId: 7,
+    productType: "Furniture",
+    productName: "Gejst Shelf A Black Ash/Black",
+    productPrice: 105,
+    productTexture: "Wood",
+    productWeightInLbs: 10,
+    productSize: "60cm x 20cm",
+    firstProductImage:
+      "public/assets/product-images/gejst-shelf-a-black-ash-black/gejst-shelf-a-black-ash-black-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/gejst-shelf-a-black-ash-black/gejst-shelf-a-black-ash-black-2",
+    thirdProductImage:
+      "public/assets/product-images/gejst-shelf-a-black-ash-black/gejst-shelf-a-black-ash-black-3.webp",
+  },
+  {
+    productId: 8,
+    productType: "Lamps",
+    productName: "Golden Modern Light",
+    productPrice: 210,
+    productTexture: "Plastic",
+    productWeightInLbs: 6,
+    productSize: "20cm x 20cm",
+    firstProductImage:
+      "public/assets/product-images/golden-modern-light/golden-modern-light-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/golden-modern-light/golden-modern-light-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/golden-modern-light/golden-modern-light-3.webp",
+  },
+  {
+    productId: 9,
+    productType: "Lamps",
+    productName: "Lamp Light Blue",
+    productPrice: 98,
+    productTexture: "Plastic",
+    productWeightInLbs: 8,
+    productSize: "14cm x 14cm",
+    firstProductImage:
+      "public/assets/product-images/lamp-light-blue/lamp-light-blue-1.png",
+    secondProductImage:
+      "public/assets/product-images/lamp-light-blue/lamp-light-blue-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/lamp-light-blue/lamp-light-blue-3.webp",
+  },
+  {
+    productId: 10,
+    productType: "Chairs",
+    productName: "Little Woolen Armchair ",
+    productPrice: 520,
+    productTexture: "Wool",
+    productWeightInLbs: 50,
+    productSize: "60cm x 60cm",
+    firstProductImage:
+      "public/assets/product-images/little-armchair-sheepskin/little-armchair-sheepskin-1.jpg",
+    secondProductImage:
+      "public/assets/product-images/little-armchair-sheepskin/little-armchair-sheepskin-2.jpg",
+    thirdProductImage:
+      "public/assets/product-images/little-armchair-sheepskin/little-armchair-sheepskin-3.jpg",
+  },
+  {
+    productId: 11,
+    productType: "Lamps",
+    productName: "Mat Black Lamp",
+    productPrice: 115,
+    productTexture: "Plastic",
+    productWeightInLbs: 8,
+    productSize: "12cm x 12cm",
+    firstProductImage:
+      "public/assets/product-images/mat-black-lamp/mat-black-lamp-1.png",
+    secondProductImage:
+      "public/assets/product-images/mat-black-lamp/mat-black-lamp-2.webp",
+    thirdProductImage:
+      "public/assets/product-images/mat-black-lamp/mat-black-lamp-3.webp",
   },
 ];
