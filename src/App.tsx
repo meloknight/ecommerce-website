cd7b1f5ea813f105789a13ac11055dd05217d4ef
brody <49502879+meloknight@users.noreply.github.com>
2024-03-18 16:34:29 -0700

    continue work on the product page

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,67 @@ function App() {
       <section className="product-page-container">
         <div className="product-page-product-container">
           <div className="product-page-top-container">
-            <div className="product-page-top-left"></div>
-            <div className="product-page-top-right"></div>
+            <div className="product-page-top-left">
+              <div className="product-page-main-image-container">
+                <img
+                  src="/assets/product-images/black-and-white-lamp/black-and-white-lamp-1.jpeg"
+                  alt=""
+                />
+              </div>
+              <div className="product-page-secondary-images-container">
+                <div className="first-product-image-container">
+                  <img
+                    src="/assets/product-images/black-and-white-lamp/black-and-white-lamp-1.jpeg"
+                    alt=""
+                  />
+                </div>
+                <div className="second-product-image-container">
+                  <img
+                    src="/assets/product-images/black-and-white-lamp/black-and-white-lamp-1.jpeg"
+                    alt=""
+                  />
+                </div>
+                <div className="third-product-image-container">
+                  <img
+                    src="/assets/product-images/black-and-white-lamp/black-and-white-lamp-1.jpeg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="product-page-top-right">
+              <h1>Comfy Chair</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur voluptate dicta quas ex expedita, dolores ipsam
+                voluptatibus, exercitationem nostrum vero nemo praesentium error
+                quos sint.
+              </p>
+              <div className="product-page-quantity-container">
+                <div>Quantity</div>
+                <div>-1+</div>
+                <div>$111.00</div>
+              </div>
+              <div className="product-page-button-container">
+                <button>ADD TO CART</button>
+                <button>BUY NOW</button>
+              </div>
+            </div>
           </div>
-          <div className="product-page-bottom-container"></div>
+          <div className="product-page-bottom-container">
+            <div className="product-texture-container">
+              <h2>Texture:</h2>
+              <p>Comfy Material</p>
+            </div>
+            <div className="product-texture-container">
+              <h2>Texture:</h2>
+              <p>Comfy Material</p>
+            </div>
+            <div className="product-texture-container">
+              <h2>Texture:</h2>
+              <p>Comfy Material</p>
+            </div>
+          </div>
         </div>
       </section>
     );
